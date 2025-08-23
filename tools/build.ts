e9083470975dd00e700815a4ed61067ea1f0739e
ericchase <github@eric.addymail.com>
2025-08-23 18:28:30 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/build.ts
+++ tools/build.ts
@@ -2,12 +2,13 @@ import { BunPlatform_Argv_Includes } from '../src/lib/ericchase/BunPlatform_Argv
 import { Step_Dev_Format } from './core-dev/step/Step_Dev_Format.js';
 import { Step_Dev_Project_Update_Config } from './core-dev/step/Step_Dev_Project_Update_Config.js';
 import { Builder } from './core/Builder.js';
+import { Processor_Merge_Files } from './core/processor/Processor_Merge_Files.js';
 import { Processor_Set_Writable } from './core/processor/Processor_Set_Writable.js';
 import { Processor_TypeScript_Generic_Bundler } from './core/processor/Processor_TypeScript_Generic_Bundler.js';
 import { Step_Bun_Run } from './core/step/Step_Bun_Run.js';
 import { Step_FS_Clean_Directory } from './core/step/Step_FS_Clean_Directory.js';
+import { Step_FS_Copy_Files } from './core/step/Step_FS_Copy_Files.js';
 import { Step_FS_Move_Files } from './core/step/Step_FS_Move_Files.js';
-import { Step_Output_Merge_Files } from './core/step/Step_Output_Merge_Files.js';
 import { Processor_JavaScript_Rollup } from './lib-vscode-extension/processors/Processor_JavaScript_Rollup.js';
 import { Step_VSCE_Package } from './lib-vscode-extension/steps/Step_VSCE_Package.js';
 
@@ -41,7 +42,7 @@ const external = [
 
 Builder.SetProcessorModules(
   // Bundle the IIFE scripts and module scripts.
-  Processor_TypeScript_Generic_Bundler({ target: 'node' }, { bundler_mode: 'iife' }),
+  // Processor_TypeScript_Generic_Bundler({ target: 'node' }, { bundler_mode: 'iife' }),
   Processor_TypeScript_Generic_Bundler({ external, target: 'node' }, { bundler_mode: 'module' }),
   Processor_JavaScript_Rollup({ external }),
   // Write non-bundle and non-library files.
@@ -57,21 +58,15 @@ Builder.SetProcessorModules(
       `original-repo/syntaxes/**`,
       `original-repo/LICENSE`,
       `original-repo/BACKERS.md`,
-      `original-repo/CHANGELOG.md`,
-      `original-repo/package.json`,
       //
     ],
     value: true,
   }),
-  //
-);
-
-Builder.SetCleanUpSteps(
-  Step_Output_Merge_Files(
+  Processor_Merge_Files(
     {
       type: 'json',
-      merge_files: ['original-repo/package.json', 'package.json'],
-      out_file: 'package.json',
+      merge_paths: ['original-repo/package.json', 'package.json'],
+      out_path: 'package.json',
       modify: (data: any) => {
         // remove the spyware
         delete data.contributes.configuration.properties['code-runner.enableAppInsights'];
@@ -83,10 +78,24 @@ Builder.SetCleanUpSteps(
     },
     {
       type: 'text',
-      merge_files: ['CHANGELOG.md', 'original-repo/CHANGELOG.md'],
-      out_file: 'CHANGELOG.md',
+      merge_paths: ['CHANGELOG.md', 'original-repo/CHANGELOG.md'],
+      out_path: 'CHANGELOG.md',
     },
   ),
+  //
+);
+
+Builder.SetAfterProcessingSteps(
+  Step_FS_Copy_Files({
+    include_patterns: ['**'],
+    from_dir: `${Builder.Dir.Out}/original-repo`,
+    into_dir: Builder.Dir.Out,
+    overwrite: true,
+  }),
+  //
+);
+
+Builder.SetCleanUpSteps(
   Step_FS_Move_Files({
     include_patterns: ['**'],
     from_dir: `${Builder.Dir.Out}/original-repo`,
